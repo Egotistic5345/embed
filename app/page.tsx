d7fbfe27a1b87db604487394311582a892f65d18
artemgladkov <artem.gladkovw@gmail.com>
2026-04-24 09:22:18 +0300

    added env

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <main
           className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <iframe
-            src="http://localhost:4000/embed/6ccb077a-6675-4179-a52d-cbad7cefcebf?autoplay=1&muted=1"
+            src={process.env.NEXT_PUBLIC_IFRAME_URL}
             width="640" height="360"
             frameBorder="0"
             allow="autoplay; fullscreen"
